85a45c340fe303b094a64e38503d3bc9a2471ee5
Daniel Teixeira <115299209+DanielTeixeira23@users.noreply.github.com>
2023-02-02 20:22:39 -0300

    Add files via upload

---
+++ Questão 1.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <locale.h>
+
+void mostrar_vetor(int num[], int tam);
+void selection_sort(int num[], int tam);
+
+int main(){
+	setlocale(LC_ALL, "Portuguese_Brazil");
+	
+	int idades[10];
+	
+	printf("\t\t\tDIGITE 10 IDADES E VEJA ELAS EM ORDEM CRESCENTE\n\n");
+	
+	for(int i=0; i<10; i++){
+		printf("Digite a %d° idade: ", i+1);
+		scanf("%d", &idades[i]);
+	}
+	
+	selection_sort(idades, 10);
+	mostrar_vetor(idades, 10);
+	
+	return 0;	
+}
+
+void mostrar_vetor(int num[], int tam){
+	for(int i=0; i<tam; i++){
+		printf("%d ", num[i]);
+	}
+	printf("\n");
+}
+
+void selection_sort(int num[], int tam){
+	int i, j, min, aux;
+	for(i=0; i<tam; i++){
+		min = i;
+		for(j= (i+1); j<tam; j++){
+			if(num[j] < num[min]){
+				min=j;
+			}
+		}
+		
+		if(i != min){
+			aux = num[i];
+			num[i] =  num[min];
+			num[min] = aux;
+		}
+	}
+}
